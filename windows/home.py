91c39984e27af156d3b63c95c2f0cf6afb712c20
Mohammed Ehsan <mohammedehsan363@gmail.com>
2025-12-22 04:14:26 +0530

    homepage card in prog

--- windows/home.py
+++ windows/home.py
@@ -1,13 +1,13 @@
 # Importing modules from PySide6 library
-from PySide6.QtWidgets import QMainWindow, QWidget, QPushButton, QLabel, QVBoxLayout, QToolBar
+from PySide6.QtWidgets import QMainWindow, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QToolBar, QFrame
 from PySide6.QtGui import QAction, QFont, QIcon
 from PySide6.QtCore import Qt
-# Homepage window class
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('FundTrack') # Title of the window
 
+        self.setWindowTitle('FundTrack') # Title of the window
         # Window size
         self.resize(1920, 1080)
         self.setMinimumSize(1170, 650)
@@ -20,21 +20,73 @@ class MainWindow(QMainWindow):
         font.setPointSize(26)
         font.setBold(True)
 
-        # Create UI elements
+        # Layout
+        pageLayout = QVBoxLayout()
+        pageLayout.setAlignment(Qt.AlignTop)
+        pageLayout.setSpacing(15)
 
+        topRow = QHBoxLayout()
+        topRow.setAlignment(Qt.AlignLeft)
+        topRow.setSpacing(20)
+
+        # Create UI elements
         self.heading = QLabel("HomePage")
         self.heading.setAlignment(Qt.AlignLeft)
         self.heading.setStyleSheet("""
-        font-size: 36px;
-        padding-top: 15px;
-        padding-left: 10px;
-        font-weight: bold;""")
+            font-size: 36px;
+            padding-top: 15px;
+            padding-left: 10px;
+            font-weight: bold;""")
+
+        summaryCard = QFrame() # Summary card
+        summaryCard.setFixedWidth(300)
+        summaryCard.setStyleSheet("""
+            background-color: #222222;
+            padding: 10px;
+            border-radius: 25px;
+            margin-left: 20px;""")
+
+        self.summaryLabel = QLabel('Summary')
+        self.summaryLabel.setStyleSheet("""
+            font-size: 28px;
+            color: White;
+            font-weight: bold;
+            margin-left: 30px;
+            padding-top: 5px;
+            margin-top: 20px;
+            background-color: #242424;""")
+
+        budgetFile = open('data/budget.txt','r')
+        self.budgetRead = budgetFile.readline()
+        budgetFile.close()
+
+        self.budget = f'Budget: {int(self.budgetRead):,}AED'
+        summaryLayout = QVBoxLayout(summaryCard)
+        summaryLayout.addWidget(self.summaryLabel)
+        summaryLayout.addWidget(QLabel(self.budget))
+        summaryLayout.addWidget(QLabel("Expense: 💲XXX"))
+
+        topRow.addWidget(summaryCard)
+
+
+
+        #aaaaaaaaaaaaaaaaaaa
+        graphCard = QFrame()
+        graphCard.setStyleSheet("""
+            background-color: #1f3b14;
+            border-radius: 20px;
+        """)
+
+        graphLayout = QVBoxLayout(graphCard)
+        graphLayout.addWidget(QLabel("Graph", alignment=Qt.AlignCenter))
+
+        topRow.addWidget(graphCard, stretch=2)
+        #aaaaaaaaaaaaaaaaaaa
 
         # Toolbar options
         toolbar = QToolBar("Main Toolbar", self)
         self.addToolBar(toolbar)
         toolbar.setStyleSheet('Background-color: #e78c4d; font-size: 20px;')
-
         action_add = QAction("Add Expense", self)
         toolbar.addAction(action_add)
 
@@ -54,14 +106,14 @@ class MainWindow(QMainWindow):
         toolbar.addAction(action_add)
 
         # Connect button click to function
-        #self.button.clicked.connect(self.increase_count)
+        # #self.button.clicked.connect(self.increase_count)
 
-        # Layout
-        layout = QVBoxLayout()
-        layout.addWidget(self.heading)
+        pageLayout.addWidget(self.heading)
+        pageLayout.addLayout(topRow)
+
+        pageLayout.addStretch() # <-- Should be last! To make everything in layout align to the left
 
         centralWidget = QWidget()
-        centralWidget.setLayout(layout)
+        centralWidget.setLayout(pageLayout)
         centralWidget.setStyleSheet('background-color: #141414; color: #e78c4d;')
-        self.setCentralWidget(centralWidget)
-        self.setCentralWidget(centralWidget)
+        self.setCentralWidget(centralWidget) # <-- Stuff into Central Widget
