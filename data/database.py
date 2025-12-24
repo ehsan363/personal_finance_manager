59ac8912e8defbd3ff37aad1d11a8a98364d8348
Mohammed Ehsan <mohammedehsan363@gmail.com>
2025-12-25 02:23:40 +0530

    history function added

--- data/database.py
+++ data/database.py
@@ -17,7 +17,8 @@ class DBmanager:
         month = int(today.strftime('%m'))
         year = int(today.strftime('%Y'))
 
-        cursor = self.conn.execute(f'''SELECT SUM(amount) AS total_expense
+        cursor = self.conn.execute(f'''
+            SELECT SUM(amount) AS total_expense
             FROM transactions
             WHERE type = 'expense'
             AND strftime('%Y', date) = '{year}'
@@ -28,7 +29,18 @@ class DBmanager:
 
     # Function for fetching transaction history
     def history(self):
-        pass
+        cursor = self.conn.execute('''
+            SELECT category, amount, date, type
+            FROM transactions
+            ORDER BY date DESC
+            LIMIT 5;''')
+        rawRows = cursor.fetchall()
+        rows = []
+        data = []
+        for i in rawRows:
+            rows.append([dict(i)['category'],dict(i)['amount'],dict(i)['date'],dict(i)['type']])
+
+        return rows
 
     # Function to close SQLite
     def close(self):
