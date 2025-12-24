f220d122a16268143ab726686ca6580d788025fe
Mohammed Ehsan <mohammedehsan363@gmail.com>
2025-12-25 02:26:33 +0530

    date comparing function added

--- dateAndTime.py
+++ dateAndTime.py
@@ -1,8 +1,12 @@
-from datetime import datetime
+from datetime import datetime, date
 import random
+from itertools import compress
+
+from mesonbuild.utils.universal import partition
+
 
 def tdy():
-    return datetime.today()
+    return datetime.now()
 
 def greetingText():
     time = datetime.now()
@@ -18,3 +22,26 @@ def greetingText():
         nightgreets = ["Night shift mode: ON","After hours finance check" ,"The numbers never sleep.", "Welcome back"]
         randomgreeting = random.choice(nightgreets)
         return randomgreeting
+
+def dateCompare(compareDate):
+    year = ''
+    month = ''
+    day = ''
+    for i in range(len(compareDate)):
+        if i < 4:
+            year += compareDate[i]
+        elif i < 7 and compareDate[i] != '-':
+            month += compareDate[i]
+        elif i <=  9 and compareDate[i] != '-':
+            day += compareDate[i]
+
+    today = date.today()
+    diffInDays = (date.today() - date(int(year), int(month), int(day))).days
+    if diffInDays == 0:
+        return 'Today'
+    elif diffInDays == 1:
+        return 'Yesturday'
+    elif diffInDays < 6:
+        return f'{diffInDays} days ago'
+    else:
+        return f'{day}/{month}/{year}'
